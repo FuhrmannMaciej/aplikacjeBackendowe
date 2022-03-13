df9e1f658e5b70b2541f8abbd9db2623812f616d
maciej.fuhrmann <maciej.fuhrmann@microsoft.wsei.edu.pl>
2022-03-13 09:13:47 +0100

    second exercise

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.io.Console;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Main2 {
 
     public static void main(String[] args) {
+        try (FileOutputStream outputStream = new FileOutputStream("src/com/company/file2")){
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
 
+            outputStream.write(text.getBytes(StandardCharsets.UTF_8));
+
+            System.out.println(("Gotowe"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
